59370368fb292f68678fa80d9b410c6945ed3f07
albaslug <albaslug@gmail.com>
2021-01-04 21:27:44 +0300

    app renamed to Drawme

--- src/main/java/org/albaslug/drawme/DrawmeApplication.java
+++ src/main/java/org/albaslug/drawme/DrawmeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PokedrawApplication {
+public class DrawmeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PokedrawApplication.class, args);
+        SpringApplication.run(DrawmeApplication.class, args);
     }
 
 }
